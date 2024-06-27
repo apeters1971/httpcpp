0dcedb5bcc3faa5f6cf805b25f09691c6bbd7b1b
Andreas Joachim Peters <apeters@st-srv-100gb-ip301-b7bfe.cern.ch>
2024-06-27 10:55:28 +0200

    Add seconds to progress bar

--- httprogress.hh
+++ httprogress.hh
@@ -64,8 +64,8 @@ public:
     float abs_time = ((float)((abs_stop_time.tv_sec - abs_start_time.tv_sec) * 1000
 			      +
 			      (abs_stop_time.tv_usec - abs_start_time.tv_usec) / 1000));
-    CERR(("| %.02f %% [%.01f MB/s]\r", 100.0 * bytes / size,
-	  bytes / abs_time / 1000.0));
+    CERR(("| %.02f %% [%.01f MB/s] %.02f s\r", 100.0 * bytes / size,
+	  bytes / abs_time / 1000.0, abs_time / 1000.0));
 
   }
 
